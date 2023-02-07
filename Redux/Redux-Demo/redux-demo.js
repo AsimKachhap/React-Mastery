771e0dbf62e0adfc85b8a96232abc3090baa0c11
unknown <asim26k@gmail.com>
2023-02-08 02:58:30 +0530

    fixed redux-store bug

--- Redux/Redux-Demo/redux-demo.js
+++ Redux/Redux-Demo/redux-demo.js
@@ -1,18 +1,20 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type == "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 };
 
 const store = redux.createStore(counterReducer);
 
-const counterSubsciber = () => {
+const counterSubscriber = () => {
   const latestState = store.getState();
   console.log(latestState);
 };
 
-store.subscribe(counterSubsciber);
+store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "INCREMENT" });
