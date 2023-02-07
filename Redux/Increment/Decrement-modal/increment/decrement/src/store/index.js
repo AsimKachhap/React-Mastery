4d2996e28e51380e1866f65ccb2bfd2612b2ed09
unknown <asim26k@gmail.com>
2023-02-08 04:09:06 +0530

    provided the store to App and fixed an error in store/index.js

--- Redux/Increment/Decrement-modal/increment/decrement/src/store/index.js
+++ Redux/Increment/Decrement-modal/increment/decrement/src/store/index.js
@@ -14,6 +14,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
